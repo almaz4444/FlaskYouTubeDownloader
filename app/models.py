d694bd5b5074c83176dfcbfa7cbb884304e98b75
almaz4444 <almaz4444f@gmail.com>
2023-05-19 00:21:50 +0500

    added answer to comments; changed video loads

--- app/models.py
+++ app/models.py
@@ -3,29 +3,30 @@ from datetime import datetime
 from . import db
 
 class Video:
-	def __init__(self, url, name, path, static_path, res, mime_type, file_size):
+	def __init__(self, url, name, static_path, res, mime_type, file_size):
 		self.url = url
 		self.name = name
-		self.path = path
 		self.static_path = static_path
 		self.res = res
 		self.mime_type = mime_type
 		self.file_size = file_size
 
-	def __repr__(self):
-		return """
-URL: {}
-Name: {}
-Path: {}
-Static path: {}
-Resolution: {}
-Mime type: {}
-File size: {}
-""".format(self.url, self.name, self.path, self.static_path, self.res, self.mime_type, self.file_size)
+class Poster:
+	def __init__(self, path, mime_type, file_size):
+		self.path = path
+		self.mime_type = mime_type
+		self.file_size = file_size
+
+class Audio:
+	def __init__(self, path, mime_type, file_size):
+		self.path = path
+		self.mime_type = mime_type
+		self.file_size = file_size
+
 
 class Comment(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	author = db.Column(db.String(50), nullable=False)
 	text = db.Column(db.String(255), nullable=False)
-	likes_count = db.Column(db.Integer, default=0)
 	date = db.Column(db.DateTime, default=datetime.utcnow)
+	answer_to_id = db.Column(db.Integer, default=0)
--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request, send_file, redirect, flash, m
 from pytube import YouTube
 
 from . import app, db
-from .models import Video, Comment
+from .models import Video, Poster, Audio, Comment
 from .util import *
 
 
@@ -22,39 +22,45 @@ def download_video():
     url = request.args.get("url")
     resolution = request.args.get("resolution")
 
-    main_video_stream = YouTube(url).streams.filter(progressive=True)[-1]
-    is_main_video = main_video_stream.resolution == resolution
-    video = get_video(url, resolution, not is_main_video)
-    video_name = video.get_file_path().replace(os.getcwd(), '')[1::]
-    path = f"static\\videos\\({resolution}) {video_name}"
+    yt = YouTube(url)
+    streams = yt.streaming_data['formats'][::-1]
+    stream = list(filter(lambda data: data['qualityLabel'] == resolution, streams))[0]
+    video_path, video_filesize = get_video(stream, yt.title)
 
-    return send_file(path, as_attachment=True)
+    return send_file(video_path.replace('app\\', ''), as_attachment=True)
+
+@app.route('/download/audio', methods=['GET'])
+def download_audio():
+    url = request.args.get("url")
+
+    yt = YouTube(url)
+    streams = yt.streaming_data['formats'][::-1]
+    audio = get_audio(get_video_id_by_url(url), YouTube(url).title)
+
+    return send_file(poster_path, as_attachment=True), del_poster_th.start()
     
 @app.route('/download/poster', methods=['GET'])
 def download_poster():
     url = request.args.get("url")
     poster = get_poster(get_video_id_by_url(url), YouTube(url).title)
-    poster_path = f"static\\{poster}"
-
-    del_poster_th = Thread(target=remove_file_for, args=(5, f"app\\{poster_path}"))
+    poster_path = f"static\\{poster.path}"
 
     return send_file(poster_path, as_attachment=True), del_poster_th.start()
 
 @app.route('/search', methods=['GET'])
 def search_video():
     url = request.args.get('url')
-    is_no_load = request.args.get('IS_NO_LOAD')
+    answer_to_id = request.args.get('answer_to_id')
+
+    if not answer_to_id:
+        answer_to_id = 0
     
-    if not is_no_load:
-        videos, main_video_index = load_videos(url)
-        if videos:
-            video_path = videos[main_video_index].static_path
-            debug(video_path)
-            poster_path = get_poster(get_video_id_by_url(url), get_name_by_path(video_path))
-            
-            return render_template('index.html', videos=videos, poster_path=poster_path, comments=Comment.query.all())
-    
-    return render_template('index.html', comments=Comment.query.all())
+    videos, main_video_index = load_videos(url)
+    if videos:
+        video_name = videos[main_video_index].name
+        poster = get_poster(get_video_id_by_url(url), video_name)
+        
+        return render_template('index.html', videos=videos, poster=poster, answer_to_id=int(answer_to_id), comments=Comment.query.all())
 
 @app.route('/send_comment', methods=['GET', 'POST'])
 def send_comment():
@@ -65,24 +71,34 @@ def send_comment():
 
         if len(comment_text.split(':')) != 1:
             author, *text = comment_text.split(':')
+            text = ':'.join(text)
         else:
             text = comment_text
 
-        if text:
+        if len(text.strip()):
             comment = Comment()
             comment.author = author
-            comment.text = ':'.join(text)
+            comment.text = text
 
             db.session.add(comment)
             db.session.commit()
-
-            response = make_response()
-            response.headers['IS_NO_LOAD'] = 'True'
             
-            return redirect(request.headers.get('Referer'), Response=response)
+        return redirect(request.headers.get('Referer'))
     else:
-        return render_template('index.html', comments=Comment.query.all())
-    
+        return redirect('/')
+
+@app.route('/answer', methods=['GET', 'POST'])
+def answer_comment():
+    if request.method == 'GET':
+        answer_id = request.args.get('comment_id')
+        url = request.headers.get('Referer')
+
+        if answer_id:
+            url += f'&answer_to_id={answer_id}'
+
+        return redirect(url)
+    else:
+        return redirect('/')
 
 @app.route('/get_action', methods=['POST'])
 def get_action():
@@ -102,19 +118,24 @@ def load_videos(url):
     videos = []
     main_video_index = 0
 
-    for i, stream in enumerate(yt.streams.filter(progressive=True)[::-1]):
-        video = yt.streams.get_by_itag(stream.itag)
+    streams = yt.streaming_data['formats'][::-1]
+
+    for i, stream in enumerate(streams):
+        res = stream["qualityLabel"]
+        mime_type = stream["mimeType"].split(";")[0].split("/")[-1]
 
         if i == 0:
-            video = get_video(url, video.resolution, False)
+            video_path, video_filesize = get_video(stream, yt.title, mime_type)
+        else:
+            head = requests.head(stream['url'])
+            video_filesize = int(head.headers.get('Content-Length', 0))
 
-        videos.append(Video(url,
-                            f'({video.resolution}) {yt.title}',
-                            video.get_file_path(),
-                            get_static_path(video.get_file_path(), video.resolution),
-                            video.resolution,
-                            video.mime_type.split("/")[-1], 
-                            human_format(video.filesize))
+        videos.append(Video(url = url,
+                            name = f'({res}) {yt.title}',
+                            static_path = video_path.replace('app\\static', '').replace('\\', '/'),
+                            res = res,
+                            mime_type = mime_type, 
+                            file_size = human_format(video_filesize))
                     )
 
     return videos, main_video_index
@@ -122,17 +143,36 @@ def load_videos(url):
 def complete_download_function(stream, file_path):
     Thread(target=remove_file_for, args=(5, file_path)).start()
     
-def get_video(url, resolution, is_deletable=True):
-    yt = YouTube(url, on_complete_callback=complete_download_function if is_deletable else None)
+def get_video(stream: dict, name: str, mime_type: str):
+    path = f'app\\static\\videos\\({stream["qualityLabel"]}) {shielding(name)}.{mime_type}'
+    filesize = 0
 
-    video = yt.streams.filter(res=resolution).first()
-    video.download(output_path="app\\static\\videos", filename_prefix=f"({video.resolution}) ")
-    
-    return video
+    response = requests.get(stream['url'])
 
+    with open(path, "wb") as file:
+        file.write(response.content)
+        filesize = file.__sizeof__()
+
+    return path, filesize
+
+def get_audio(stream: dict, name: str, mime_type: str):
+    path = f'app\\static\\audio\\({stream["qualityLabel"]}) {shielding(name)}.{mime_type}'
+    filesize = 0
+
+    response = requests.get(stream['url'])
+
+    with open(path, "wb") as file:
+        file.write(response.content)
+        filesize = file.__sizeof__()
+
+    return Audio(path, mime_type, filesize)
 
 def get_poster(video_id, name):
     fielname = shielding(name.replace('(720p) ', '').replace(".mp4", ""))
-    with open(f"app/static/images/{fielname}.jpg", "wb") as f:
-        f.write(requests.get(posters_get_url.format(video_id)).content)
-    return f'images/{fielname}.jpg'
+    filesize = 0
+
+    with open(f"app/static/images/{fielname}.jpg", "wb") as file:
+        file.write(requests.get(posters_get_url.format(video_id)).content)
+        filesize = file.__sizeof__()
+
+    return Poster(f'images/{fielname}.jpg', 'jpg', human_format(filesize))
